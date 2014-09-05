d83473319169508e9fe737dad37a428e36e70ef4
Myles Byrne <myles@myles.id.au>
2014-09-04 18:11:09 -0700

    cleanup git metadata script

--- metadata/realtime/git.sh
+++ metadata/realtime/git.sh
@@ -1,8 +1,7 @@
-echo "git:"
-echo "  is_repo: "      "$([ -d .git ] && git rev-parse --is-inside-work-tree)"
+# exit if not in a git repo
+[ -d .git ] && [ $(git rev-parse --is-inside-work-tree) = "true" ] || exit
 
-if test $(git rev-parse --show-toplevel 2> /dev/null); then
-  echo "  repo:"     "$( (basename `git rev-parse --show-toplevel`) 2> /dev/null )"
-  echo "  branch:"      "$(git branch | grep -e '^\*' | awk '{print $2}')"
-  echo "  remote: "     "$(git remote -v  2> /dev/null | head -1 | awk '{print $2}')"
-fi
+echo "git:"
+echo "  repo_folder:"  "$(basename $(git rev-parse --show-toplevel))"
+echo "  branch:"       "$(git branch | grep -e '^\*' | awk '{print $2}')"
+echo "  remote:"       "$(git remote -v | head -1 | awk '{print $2}')"
