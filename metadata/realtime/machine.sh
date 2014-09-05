12f4e9f5dc7386e5f119d2ac58080b2a93a9c010
Myles Byrne <myles@myles.id.au>
2014-09-04 18:13:38 -0700

    cleanup machine metadata

--- metadata/realtime/machine.sh
+++ metadata/realtime/machine.sh
@@ -1,20 +1,20 @@
 echo "machine: "
 
-echo "  hostname: "     $(hostname)
+echo "  hostname:"     $(hostname)
 
-echo "  uname: "
+echo "  uname:"
 # -a Behave as though all of the options -mnrsv were specified.
-echo "    raw: "        $(uname -a)
+echo "    raw:"        $(uname -a)
 # -m print the machine hardware name.
-echo "    hardware: "   $(uname -m)
+echo "    hardware:"   $(uname -m)
 # -n print the nodename (the nodename may be a name that the system is known by to a communications network).
-echo "    nodename: "   $(uname -n)
+echo "    nodename:"   $(uname -n)
 # -p print the machine processor architecture name.
-echo "    processor: "  $(uname -p)
-echo "    os: "
+echo "    processor:"  $(uname -p)
+echo "    os:"
 # -r print the operating system release.
-echo "      release: "  $(uname -r)
+echo "      release:"  $(uname -r)
 # -s print the operating system name.
-echo "      name: "    $(uname -s)
+echo "      name:"     $(uname -s)
 # -v print the operating system version.
-echo "      version: " $(uname -v)
+echo "      version:"  $(uname -v)
